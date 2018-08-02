ee8ce1cfda971e357de8ab0a0b30f8e0b28e1323
Daniel Sundman <daniel@yds.se>
2018-08-02 16:24:57 -0700

    refactored Quote out of App.js

---
+++ app/QuoteRandomizer/src/Quote.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
+
+
+const quotes=[
+  'Don\'t wait for opportunity. Create it.',
+  'Now, if you two don\'t mind, I\'m going to bed before either of you come up with another clever idea to get us killed or worse, expelled!'
+]
+export default class Quote extends React.Component {
+  state={
+    index:getRandomInt(quotes.length)
+  }
+
+  render() {
+    const quote = quotes[this.state.index]
+    return (
+      <View style={styles.container}>
+        <Text>{quote}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
