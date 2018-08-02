acbe3c8de2e79affe9c3eac6bc922087b981d873
Daniel Sundman <daniel@yds.se>
2018-08-02 15:55:56 -0700

    added getRandomInt function

--- app/QuoteRandomizer/App.js
+++ app/QuoteRandomizer/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
+
+
 const quotes=[
   'Don\'t wait for opportunity. Create it.',
   'Now, if you two don\'t mind, I\'m going to bed before either of you come up with another clever idea to get us killed or worse, expelled!'
@@ -11,7 +14,7 @@ export default class App extends React.Component {
   }
 
   render() {
-const quote = quotes[Math.round(Math.random())]
+    const quote = quotes[getRandomInt(quotes.length)]
     return (
       <View style={styles.container}>
         <Text>{quote}</Text>
