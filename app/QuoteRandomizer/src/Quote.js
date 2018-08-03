175c42e731cb6dfec1921c4cd5d5aade065bfc7c
Daniel Sundman <daniel@yds.se>
2018-08-03 16:49:18 -0700

    use class property for button callback

--- app/QuoteRandomizer/src/Quote.js
+++ app/QuoteRandomizer/src/Quote.js
@@ -4,40 +4,40 @@ import { Button, Text } from 'native-base';
 
 const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
 
-const quotes=[
-  'Don\'t wait for opportunity. Create it.',
-  'Now, if you two don\'t mind, I\'m going to bed before either of you come up with another clever idea to get us killed or worse, expelled!'
+const quotes = [
+	"Don't wait for opportunity. Create it.",
+	"Now, if you two don't mind, I'm going to bed before either of you come up with another clever idea to get us killed or worse, expelled!"
 ];
 
 export default class Quote extends React.Component {
-  state={
-    index:getRandomInt(quotes.length)
-  }
+	state = {
+		index: getRandomInt(quotes.length)
+	};
 
-  newRandomQuote() {
-    this.setState({
-      index:getRandomInt(quotes.length)
-    });
-  }
+	newRandomQuote = () => {
+		this.setState({
+			index: getRandomInt(quotes.length)
+		});
+	};
 
-  render() {
-    const quote = quotes[this.state.index]
-    return (
-      <View style={styles.container}>
-        <Button block onPress={() => this.newRandomQuote()}>
-          <Text>Randomize</Text>
-        </Button>
-        <Text>{quote}</Text>
-      </View>
-    );
-  }
+	render() {
+		const quote = quotes[this.state.index];
+		return (
+			<View style={styles.container}>
+				<Button block onPress={this.newRandomQuote}>
+					<Text>Randomize</Text>
+				</Button>
+				<Text>{quote}</Text>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
