b944b122f663109054278ffa32510da2863695a8
Daniel Sundman <daniel@yds.se>
2018-08-03 16:32:42 -0700

    added button to get new random quote

--- app/QuoteRandomizer/src/Quote.js
+++ app/QuoteRandomizer/src/Quote.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Button, Text } from 'native-base';
 
 const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
-
 
 const quotes=[
   'Don\'t wait for opportunity. Create it.',
   'Now, if you two don\'t mind, I\'m going to bed before either of you come up with another clever idea to get us killed or worse, expelled!'
-]
+];
+
 export default class Quote extends React.Component {
   state={
     index:getRandomInt(quotes.length)
+  }
+
+  newRandomQuote() {
+    this.setState({
+      index:getRandomInt(quotes.length)
+    });
   }
 
   render() {
     const quote = quotes[this.state.index]
     return (
       <View style={styles.container}>
+        <Button block onPress={() => this.newRandomQuote()}>
+          <Text>Randomize</Text>
+        </Button>
         <Text>{quote}</Text>
       </View>
     );
