a2b3753989a570f3974727e34d779aedf7fbdd09
Daniel Sundman <daniel@yds.se>
2018-08-03 19:06:41 -0700

    load fonts to handle android properly

--- app/QuoteRandomizer/App.js
+++ app/QuoteRandomizer/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import Quote from './src/Quote.js'
+import * as Expo from 'expo';
+import Quote from './src/Quote.js';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <Quote />
-    );
-  }
+	state = {
+		isReady: false
+	};
+
+	componentWillMount() {
+		this.loadFonts();
+	}
+
+	async loadFonts() {
+		await Expo.Font.loadAsync({
+			Roboto: require('native-base/Fonts/Roboto.ttf'),
+			Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf')
+		});
+		this.setState({ isReady: true });
+	}
+
+	render() {
+		if (!this.state.isReady) {
+			return <Expo.AppLoading />;
+		}
+
+		return <Quote />;
+	}
 }
