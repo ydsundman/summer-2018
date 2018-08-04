520445921f9d01739e3aaef5b2aef8fade227934
Daniel Sundman <daniel@yds.se>
2018-08-03 17:04:57 -0700

    externalized quotes

--- app/QuoteRandomizer/src/Quote.js
+++ app/QuoteRandomizer/src/Quote.js
@@ -2,12 +2,9 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Button, Text } from 'native-base';
 
-const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
+import quotes from '../data/quotes.json';
 
-const quotes = [
-	"Don't wait for opportunity. Create it.",
-	"Now, if you two don't mind, I'm going to bed before either of you come up with another clever idea to get us killed or worse, expelled!"
-];
+const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
 
 export default class Quote extends React.Component {
 	state = {
